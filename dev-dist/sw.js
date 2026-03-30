5047d416095760958c5bd8a261f1b4886a21f2e4
kouki0514 <koukikuwahara0514@gmail.com>
2026-03-30 13:58:43 +0900

    Claude AI統合：ポストフロップ強化・APIキー対応

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.7e7q5r7cjlc"
+    "revision": "0.4gtbqinlntc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
